a5d8dd0f4622d00d574496f2ffe2ae145545221b
Ira Miller <public@iramiller.com>
2018-07-01 06:00:48 -0500

    list self by default

--- cli.js
+++ cli.js
@@ -1,34 +1,130 @@
 #!/usr/bin/env node
+const { getAddress } = require('guld-mail')
+const { getName } = require('guld-user')
+const { generate, listKeys, listSecretKeys, getPublicKey, getSecretKey } = require('keyring-gpg')
+const { setupConfig, getConfig } = require('guld-git-config')
+const { setSecureDefaults, setDefaultKey } = require('gpg-conf')
 const program = require('commander')
-const pkg = require('./package.json')
-const VERSION = pkg.version
-const NAME = Object.keys(pkg.bin)[0]
+const VERSION = require('./package.json').version
+const inquirer = require('inquirer')
+var guldname
+var guldmail
 
-const COMMANDS = {
-  'config': ['Manage git config files the guld way.', {isDefault: true}],
-  'env': ['Guld environment detection module.'],
-  'git': ['Guld standardized Command Line Interface (CLI) for git.']
-}
-
+/* eslint-disable no-console */
 program
-  .name('guld')
+  .description('Cryptographic key storage and usage.')
   .version(VERSION)
-  .description('Guld decentralized internet CLI.')
-  .option('-u, --user', 'The user name to set up.')
-  .option('-r, --recipient', 'The recipient of a message or transaction.')
-  .option('-f, --fingerprint', 'The PGP fingerprint to sign with.')
-  // .option('-q, --quiet', '')
-
-for (var cmd in COMMANDS) {
-  var cmds = COMMANDS[cmd]
-  var desc = cmds.shift()
-  if (cmds.length > 0) {
-    program.command(cmd, desc, ...cmds).description(desc)
-  } else {
-    program.command(cmd, desc).description(desc)
-  }
-}
+  .option('-s, --secret', 'Perform action on secret keys.')
+  // .option('-u, --user', 'Use a specific key for the operation.')
+  .option('-a, --ascii', 'ASCII armor any output.')
+  .option('-f, --file', 'The file to read as input.')
+  .option('-o, --out', 'The file to write as output.')
+program
+  .command('init [fingerprint]')
+  .description('Intialize a PGP key with the guld network.')
+program
+  .command('list')
+  .description('List PGP keys as fingerprint UID pairs. ("*" for all)')
+program
+  .command('import')
+  .description('Import a PGP key.')
+program
+  .command('export')
+  .description('Export a PGP key.')
 
 program.parse(process.argv)
 
-module.exports = program
+async function inquireSecKey (fpr) {
+  guldname = guldname || await getName()
+  guldmail = guldmail || await getAddress()
+  try {
+    var seckeys = await listSecretKeys()
+    var fprs = Object.keys(seckeys)
+    var i = fprs.indexOf(fpr)
+    var choices = fprs.map(f => `${f} ${seckeys[f]}`)
+    inquirer
+      .prompt([
+        {
+          name: 'signingkey',
+          type: 'list',
+          message: 'Which of these is your PGP key id?',
+          choices: choices,
+          default: i
+        }
+      ])
+      .then(async answers => {
+        fpr = answers.signingkey.split(' ')[0]
+        await setupConfig({
+          user: {signingkey: fpr},
+          commit: {gpgsign: true}
+        }, guldname)
+        await setDefaultKey(fpr)
+        await setSecureDefaults()
+      })
+  } catch (e) {
+    var fpr = generate({
+      userIds: [{
+        name: guldname,
+        email: guldmail
+      }]
+    })
+    await setupConfig({
+      user: {signingkey: fpr},
+      commit: {gpgsign: true}
+    }, guldname)
+    await setDefaultKey(fpr)
+    await setSecureDefaults()
+  }
+}
+
+function joinPrint(line) {
+  console.log(Object.keys(line).map(l => `${l} ${line[l]}`).join('\n'))
+}
+
+async function exportKey() {
+  guldname = guldname || await getName()
+  guldmail = guldmail || await getAddress()
+  var cfg = await getConfig('merged', guldname)
+  if (program.secret) getSecretKey(cfg.user.signingkey).then(console.log)
+  else getPublicKey(cfg.user.signingkey).then(console.log)
+}
+
+var cmd = program.args.shift()
+/* eslint-disable no-console */
+switch (cmd) {
+  case 'init':
+    if (program.args.length > 0) inquireSecKey(program.args[0])
+    else {
+      getConfig().then(cfg => {
+        if (cfg && cfg.user && cfg.user.signingkey) inquireSecKey(cfg.user.signingkey)
+        else {
+          inquireSecKey()
+        }
+      })
+    }
+    break
+  case 'export':
+    if (program.args.length > 0) {
+      if (program.secret) getSecretKey(program.args[0]).then(console.log)
+      else getPublicKey(program.args[0]).then(console.log)
+    } else {
+      exportKey()
+    }
+    break
+  case 'list':
+  default:
+    if (program.args[0] === '*') {
+      if (program.secret) listSecretKeys().then(joinPrint)
+      else listKeys().then(joinPrint)
+    } else if (program.args.length > 0) {
+      listKeys(program.args[0]).then(joinPrint)
+    } else {
+      getName().then(async guldname => {
+        var conf = await getConfig('merged', guldname)
+        if (program.secret) listSecretKeys(conf.user.signingkey).then(joinPrint)
+        else listKeys(conf.user.signingkey).then(joinPrint)
+      })
+    }
+    break
+}
+/* eslint-enable no-console */
